9b990228e1fc62f4b194391fa067af86281db343
Mellad Sultani <140553611+takebackreality@users.noreply.github.com>
2025-08-29 00:15:35 -0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-COM_PORT = "COM65"  # change to your Nano’s port, e.g. "/dev/ttyUSB0" on Linux
-BAUD_RATE = 9600
-
-LOWER_HSV = (40, 70, 70)
-UPPER_HSV = (80, 255, 255)
-
-FRAME_WIDTH = 640
-FRAME_HEIGHT = 480
